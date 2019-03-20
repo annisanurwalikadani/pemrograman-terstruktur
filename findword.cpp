656ff87e49d60cc9195e17bd46cb8af7a911c449
syelaseptania <48612589+syelaseptania@users.noreply.github.com>
2019-03-20 20:10:01 +0700

    Update findword.cpp
    1.mengganti program
    2.menambahkan fungsi horizontal

--- findword.cpp
+++ findword.cpp
@@ -1,9 +1,53 @@
-if(hasil>0){
-  cout<<"ADA"<<endl;
-}
-else{
-  cout<<"TIDAK ADA"<<endl;
-  }
-}
+#include<iostream>
+#include<cstring>
+using namepace std;
+int cari_kata(*(word));
+{
+  int pjg,cek,hasil;
+  hasil=0;
+  pjg=strln(word);
+  
+  //HORIZONTAL KE KANAN (dari kiri ke kanan)
+              for (int k=0;k<pjg;k++){
+                if (word[k]==kata[i][j+k]){
+                  cek=k;
+                  
+                }
+                else{
+                  break;
+                }
+              }
+              if (cek==pjg-1){
+                hasil+=1;
+              }
+              else{
+                hasil+=0;
+              }
+              cek=0;
+  
+  
+  
+  
+  //HORIZONTAL KE KIRI(dari kanan ke kiri)
+              for(int k=0;k<pjg;k++){
+                if (word[k]==kata[i][j-k]){
+                  cek=k;
+                }
+                else{
+                  break;
+                }
+              }
+              if (cek==pjg-1){
+                hasil+=1;
+              }
+              else{
+                hasil+=0;
+              }
+              cek=0;
+  
+ 
+                  
+  
+
 
 
